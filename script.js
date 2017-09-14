f9901872d8ecd9d4eef251b3663e18a26b913553
Edwin Chen <edwinchen85@gmail.com>
2017-09-14 22:36:47 +0800

    Add click event listeners for modalBtn, closeBtn and window

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ var modal = document.getElementById('simpleModal');
 var modalBtn = document.getElementById('modalBtn');
 // Get close button
 var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+// Listen for open click
+modalBtn.addEventListener('click', openModal);
+// Listen for close click
+closeBtn.addEventListener('click', closeModal);
+// Listen for outside click
+window.addEventListener('click', outsideClick);
