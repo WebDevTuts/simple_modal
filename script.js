f8cee78e853c84737d7be66beb613ea5f6673d97
Edwin Chen <edwinchen85@gmail.com>
2017-09-14 22:37:16 +0800

    Add all click event handlers

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ modalBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
 // Listen for outside click
 window.addEventListener('click', outsideClick);
+
+// Function to open modal
+function openModal() {
+  modal.style.display = 'block';
+}
+
+// Function to close modal
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+// Function to close modal if outside click
+function outsideClick(e) {
+  if(e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
