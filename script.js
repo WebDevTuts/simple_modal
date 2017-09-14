09bacfbe95d605ce3581e29c1f9cdb0e43c35111
Edwin Chen <edwinchen85@gmail.com>
2017-09-14 22:35:40 +0800

    Add references to modal, modalBtn and closeBtn

---
+++ script.js
@@ -0,0 +1,6 @@
+// Get modal element
+var modal = document.getElementById('simpleModal');
+// Get open modal button
+var modalBtn = document.getElementById('modalBtn');
+// Get close button
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
